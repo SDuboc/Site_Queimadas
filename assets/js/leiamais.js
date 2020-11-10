c62e7348623bda7cc29ccc475f3e30499a0ea8e8
SDuboc <spaula@id.uff.br>
2020-11-10 19:14:44 -0300

    adiciona imagem ao resumo e página de referencias

--- assets/js/leiamais.js
+++ assets/js/leiamais.js
@@ -3,15 +3,19 @@ function leiaMais() {
     var pontos = document.getElementById("pontos")
     var maisTexto = document.getElementById("mais")
     var clickLeiaMais = document.getElementById("clickLeiaMais")
+    var menos = document.getElementById("menos")
 
-    if (pontos.style.display === "none") {
-        pontos.style.display = "inline";
+    if (maisTexto.style.display === "inline") {
+        pontos.style.display = "inline"
         maisTexto.style.display = "none";
         clickLeiaMais.innerHTML = "Leia Mais";
+        menos.style.display = "inline"
+
     } else {
         pontos.style.display = "none"
         maisTexto.style.display = "inline";
         clickLeiaMais.innerHTML = "Leia Menos";
+        menos.style.display = "none"
     }
 
 }
